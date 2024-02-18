0801c6882e1fc2c16bb68d950486d575f4cbaa6f
Zorgit <piligrimexe@gmail.com>
2024-02-18 22:59:08 +0300

    Refactor table processing method

--- Parser/src/main/java/ru/matushov/parsers/KlbmUserPageParser.java
+++ Parser/src/main/java/ru/matushov/parsers/KlbmUserPageParser.java
@@ -3,45 +3,51 @@ package ru.matushov.parsers;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.matushov.DAO.KlbmRaceData;
+import ru.matushov.entity.race.KlbmRacesData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class KlbmUserPageParser {
-    public static List<KlbmRaceData> parse(String url) {
+    public static List<KlbmRacesData> parse(String url) {
 
-        List<KlbmRaceData> raceDatas = new ArrayList<>();
+        List<KlbmRacesData> racesData = new ArrayList<>();
         try {
             var pageData = Jsoup.connect(url).userAgent("Chrome/121.0.6167.185")
                     .referrer("http://www.google.com")
                     .get();
             // Парсим страницу на таблицу с забегами
             Elements lines = pageData.select("table.table.table-condensed.table-hover tr");
-            String matchPeriod = ""; // Переменная для хранения текущего года при парсинге
-            for (Element line : lines) {
-                // Проверяем, является ли текущая строка с информацией о годе
-                if (line.select("th").size() == 1) {
-                    // Извлекаем год из строки
-                    matchPeriod = line.select("th").text();
-                    continue;
-                } else if (line.select("td:nth-child(2)").text().equals("")) {
-                    continue;
-                }
-                KlbmRaceData raceData = new KlbmRaceData();
-                //Извлекаем данные по каждому забегу
-                raceData.setDate(line.select("td:nth-child(2)").text() + " " + matchPeriod);
-                raceData.setEventName(line.select("td:nth-child(3) a").text());
-                raceData.setCity(line.select("td:nth-child(4)").text());
-                raceData.setDistance(line.select("td:nth-child(5) a").text());
-                raceData.setResult(line.select("td:nth-child(6)").text());
-                raceData.setPoints(line.select("td:nth-child(7)").text());
-                raceData.setBonuses(line.select("td:nth-child(8)").text());
-                raceDatas.add(raceData);
-            }
+            racesData = processTable(lines);
         } catch (Exception e) {
+            System.err.println("Ошибка парсинга данных со страницы:" + url);
             e.printStackTrace();
         }
-        return raceDatas;
+        return racesData;
+    }
+
+    private static List<KlbmRacesData> processTable(Elements lines) {
+        List<KlbmRacesData> racesData = new ArrayList<>();
+        String matchPeriod = ""; // Переменная для хранения текущего года при парсинге
+        for (Element line : lines) {
+            // проверка на год или шапку таблицы
+            if (line.select("th").size() == 1) {
+                matchPeriod = line.select("th").text();
+                continue;
+            } else if (line.select("td:nth-child(2)").text().equals("")) {
+                continue;
+            }
+            KlbmRacesData raceData = new KlbmRacesData();
+            //Извлекаем данные по каждому забегу
+            raceData.setDate(line.select("td:nth-child(2)").text() + " " + matchPeriod);
+            raceData.setEventName(line.select("td:nth-child(3) a").text());
+            raceData.setCity(line.select("td:nth-child(4)").text());
+            raceData.setDistance(line.select("td:nth-child(5) a").text());
+            raceData.setResult(line.select("td:nth-child(6)").text());
+            raceData.setPoints(line.select("td:nth-child(7)").text());
+            raceData.setBonuses(line.select("td:nth-child(8)").text());
+            racesData.add(raceData);
+        }
+        return racesData;
     }
 }
