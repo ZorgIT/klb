8174c35765b17202322723caa72baf4a77ec0e8f
Zorgit <piligrimexe@gmail.com>
2024-02-18 23:25:10 +0300

    Add Runner entity

---
+++ Parser/src/main/java/ru/matushov/entity/runner/KlbRunner.java
@@ -0,0 +1,11 @@
+package ru.matushov.entity.runner;
+
+import lombok.Data;
+
+@Data
+public class KlbRunner implements Runner{
+    String firstname;
+    String lastname;
+    String surname;
+    String birthday;
+}
---
+++ Parser/src/main/java/ru/matushov/entity/runner/Runner.java
@@ -0,0 +1,10 @@
+package ru.matushov.entity.runner;
+
+import java.util.Map;
+
+public interface Runner {
+    String getFirstname();
+    String getLastname();
+    String getSurname();
+    String getBirthday();
+}
