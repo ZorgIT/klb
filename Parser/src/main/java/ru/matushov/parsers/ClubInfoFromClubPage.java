9de1e7d2205c4f071b10597e77088b751849c12d
Zorgit <piligrimexe@Gmail.com>
2024-02-28 23:26:32 +0300

    Add parser club info from club page

--- Parser/src/main/java/ru/matushov/parsers/ClubInfoFromClubPage.java
+++ Parser/src/main/java/ru/matushov/parsers/ClubInfoFromClubPage.java
@@ -1,37 +1,27 @@
 package ru.matushov.parsers;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import ru.matushov.entity.club.Club;
 import ru.matushov.entity.club.ClubImpl;
 
+/*
+page example https://probeg.org/club/208/
+ */
 public class ClubInfoFromClubPage {
     public static Club parse(Document clubPage) {
         ClubImpl club = new ClubImpl();
         try {
-            club.setName(clubPage.select("h1").first().text());
+            club.setName(clubPage.getElementsByTag("h3").first().text());
             club.setEmail(clubPage.select("a[href*=mailto]").attr("href").substring(7));
-            club.setContact(clubPage.select(":containsOwn(Телефон)")
-                    .first().siblingElements().get(0).ownText().trim());
-            club.setBased(clubPage.select(":containsOwn(Дата создания)").first().ownText().split(": ")[1].trim());
-            //            clubPage.select("a[href*=vk.com]").attr("href");
+            Elements dataTable = clubPage.getElementsByClass("col-md-6");
+            //TODO указаны конкретные поля, абстрагировать
+            club.setContact(dataTable.get(0).childNode(5).toString());
+            //TODO указаны конкретные поля, абстрагировать
+            club.setBased(dataTable.get(0).childNode(7).toString());
         } catch (Exception e) {
-            System.err.println("Ошибка получения данных о клубе:");
-            e.printStackTrace();
+            System.err.println("Получены не полные данные");
         }
         return club;
     }
 }
-
-/*
-Document doc = Jsoup.connect(url).get()  парсить с жестокго диска - .parse(File())
-
-Element  fff = postDetailsDoc.getElementsByClass ("classname").first().child()
-//first - первый элемент, child - на сколько дочерних элементов спуститься вниз
-далее с fff елемента можно снять аттрибуты - .txt илил .href.
-
-Elements ffa = doc.select("a"); -получить все ссылки на странице
-Elements ffa = doc.select("a.classname"); -получить все ссылки на странице с определенным классом
-(#навание) - по id
-foreach Elements - перебирать элементы
-    element.attr("href") - каждая ссылка
- */
